0e621fb25f554d0e6c0a4ec140b67d3a1e9b242e
Suryansh Suryansh <suryanshsuryansh@Suryanshs-MacBook-Air.local>
2026-07-05 20:03:54 +0530

    transformer block stacking the attention, ffn and normalization units

---
+++ pytorch_impl/architectures/sequence/llama3/block.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from pytorch_impl.modules.attention import GQA
+from pytorch_impl.modules.feedforward import swigluFFN
+from pytorch_impl.modules.norm import RMSNorm
+from pytorch_impl.architectures.sequence.llama3.config import LlamaConfig
+
+class Block(nn.Module):
+    def __init__(self, config: LlamaConfig):
+        super().__init__()
+        self.attn = GQA(config.attention)
+        self.attn_norm = RMSNorm(config.attention.d_model, config.norm_eps)
+        self.ffn = swigluFFN(config.attention.d_model, config.ffn_multiple_of, config.ffn_dim_multiplier)
+        self.ffn_norm = RMSNorm(config.attention.d_model, config.norm_eps)
+
+    def forward(self, x, layer_num, freq_cis, padding_mask=None, KV_cache=None):
+        """
+        pre_attn_norm --> attn(with residual connection) --> pre_ffn_norm --> ffn(with residual connection)
+        """
+        h = x + self.attn(self.attn_norm(x), layer_num, freq_cis, padding_mask, KV_cache)
+        out = h + self.ffn(self.ffn_norm(h))
+        return out
